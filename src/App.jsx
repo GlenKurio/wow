977e4efb899b9b330e1c1cb128e1095f641d9b58
GlenKurio <gleb.kurip@gmail.com>
2023-12-22 13:17:53 -0600

    build/design: theme colors

--- src/App.jsx
+++ src/App.jsx
@@ -5,31 +5,35 @@ function App() {
         <summary>Some short info</summary>
         <p>Oh hello my full description. And More words without meanimg...</p>
       </details>
-      <button className="btn btn-primary">This is button</button>
-      <button className="btn btn-secondary">This is button</button>
-      <button className="btn btn-accent btn-outline">This is button</button>
-      <button className="btn-twitter">This is button</button>
+
+      <button className="btn btn-primary">DOWNLOAD</button>
+
+      <button className="btn btn-neutral  ">DOWNLOAD</button>
+      <button className="btn btn-neutral btn-outline ">DOWNLOAD</button>
+
       {/* Open the modal using document.getElementById('ID').showModal() method */}
       <button
-        className="btn btn-outline btn-accent"
+        className="btn btn-outline btn-primary btn-md"
         onClick={() => document.getElementById("my_modal_1").showModal()}
       >
         open modal
       </button>
       <dialog id="my_modal_1" className="modal ">
         <div className="modal-box ">
-          <h3 className="font-bold text-lg text-error">Hello!</h3>
-          <p className="py-4 text-warning">
+          <h3 className="font-bold text-lg text-error-content p-4 bg-error">
+            Hello!
+          </h3>
+          <p className="py-4 text-warning-content p-4 bg-warning">
             Press ESC key or click the button below to close
           </p>
-          <p className="py-4 text-info">
+          <p className="py-4 text-info-content bg-info p-4">
             Press ESC key or click the button below to close
           </p>
           <form>
             <input type="text" />
             <button onClick={(e) => e.preventDefault()}>Submit</button>
           </form>
-          <p className="py-4 text-success">
+          <p className="py-4 text-success-content p-4 bg-success">
             Press ESC key or click the button below to close
           </p>
           <div className="modal-action">
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ export default {
     fontFamily: {
       sans: "Quicksand, sans-serif",
     },
+
     extend: {},
   },
   daisyui: {
@@ -12,21 +13,44 @@ export default {
       {
         business: {
           ...require("daisyui/src/theming/themes")["business"],
-          primary: "#a991f7",
-          info: "#2563eb",
-          success: "#16a34a",
-          warning: "#E97F14",
-          error: "#ff5555",
+          primary: "#6247AA",
+          "primary-content": "#f6f4fe",
+          neutral: "#23282E",
+          "neutral-content": "#f6f4fe",
+
+          accent: "#EA6947",
+          "accent-content": "#f6f4fe",
+
+          info: "#d3e0fb",
+          "info-content": "#2563eb",
+          success: "#d4e3d2",
+          "success-content": "#29741D",
+          warning: "#fff1d1",
+          "warning-content": "#cb9513",
+          error: "#f9d3d7",
+          "error-content": "#E22137",
+          "base-100": "#202020",
         },
       },
       {
         businessLight: {
           ...require("daisyui/src/theming/themes")["business"],
-          primary: "#a991f7",
-          info: "#2563eb",
-          success: "#16a34a",
-          warning: "#E97F14",
-          error: "#ff5555",
+          primary: "#6247AA",
+          "primary-content": "#f6f4fe",
+          neutral: "#23282E",
+          "neutral-content": "#f6f4fe",
+
+          accent: "#EA6947",
+          "accent-content": "#f6f4fe",
+
+          info: "#d3e0fb",
+          "info-content": "#2563eb",
+          success: "#d4e3d2",
+          "success-content": "#29741D",
+          warning: "#fff1d1",
+          "warning-content": "#cb9513",
+          error: "#f9d3d7",
+          "error-content": "#E22137",
           "base-100": "#f6f4fe",
         },
       },
