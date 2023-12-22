a078cf02256042cf8d947c2f53b833ff8c821484
GlenKurio <gleb.kurip@gmail.com>
2023-12-21 21:54:10 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <details>
+        <summary>Some short info</summary>
+        <p>Oh hello my full description. And More words without meanimg...</p>
+      </details>
+    </>
+  );
+}
+
+export default App;
