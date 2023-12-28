848a8aa24158b1a03d81d926bb3b25a7f165ac22
GlenKurio <gleb.kurip@gmail.com>
2023-12-27 21:45:55 -0600

    build: signup with invite link

--- src/components/appComponents/auth/GoogleAuth.jsx
+++ src/components/appComponents/auth/GoogleAuth.jsx
@@ -5,7 +5,7 @@ function GoogleAuth({ prefix, roomId }) {
   return (
     <div className="flex items-center justify-center">
       <button
-        onClick={authWithGoogle({ roomId })}
+        onClick={() => authWithGoogle({ roomId })}
         className="bg-blue-600 flex items-center justify-center gap-2 py-2 px-4 rounded-md hover:bg-blue-700 text-blue-50 font-medium transition-all duration-200 active:scale-95 text-sm"
       >
         <span>
